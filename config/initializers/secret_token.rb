14ba6c09b179c632fdaf524876315cfe72ef037d
ButuzGOL <ButuzGOL@gmail.com>
2010-10-31 08:27:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ulsimag::Application.config.secret_token = 'd0d834ac4247e71286175e6a8497cc2f83ada25b8ceec60c9118854bba52b9bd7c11a64adbfd987dac92ce0105ae947e56626f03853e5c5a8e307384f9e54799'
